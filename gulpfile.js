27fc6c07cb21918839c4dc0fdb4bb247c9307942
S.Panfilov <se-panfilov@ya.ru>
2014-12-16 18:40:20 +0800

    Project basics

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var jade = require('gulp-jade');
+var watch = require('gulp-watch');
+var changed = require('gulp-changed');
+var stylus = require('gulp-stylus');
+var nib = require('nib');
+
+
+gulp.task('stylus', function () {
+    var src = 'stylus/**/*.styl';
+    var dest = 'stylus';
+
+    return gulp.src(src)
+        //.pipe(changed(dest))
+        .pipe(concat("app.styl"))
+        .pipe(stylus({use: [nib()], compress: true}))
+        .on('error', console.log)
+        .pipe(gulp.dest(dest));
+});
+
+gulp.task("jade", function () {
+    var src = 'examples/dev.jade';
+    var dest = 'examples';
+
+    return gulp.src([src])
+        .pipe(changed(dest, {extension: '.html'}))
+        .pipe(jade({pretty: true}))
+        .on('error', console.log)
+        .pipe(gulp.dest(dest));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(['examples/dev.jade'], ['jade']);
+    gulp.watch(['styles/**/*.styl'], ['stylus']);
+});
+
+gulp.task("build", function () {
+    gulp.start("jade");
+    gulp.start("stylus");
+});
+
+gulp.task("default", function () {
+    gulp.start("build");
+    gulp.start("watch");
+});
