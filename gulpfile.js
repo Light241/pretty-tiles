4ebcc92ca495c2caa69c37760664babb522d990f
S.Panfilov <se-panfilov@ya.ru>
2014-12-18 15:07:07 +0800

    added webserver

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var watch = require('gulp-watch');
 var changed = require('gulp-changed');
 var stylus = require('gulp-stylus');
 var nib = require('nib');
-
+var connect = require('gulp-connect');
 
 gulp.task('stylus', function () {
     var src = 'src/stylus/**/*.styl';
@@ -16,18 +16,20 @@ gulp.task('stylus', function () {
         .pipe(concat("pretty_tiles.styl"))
         .pipe(stylus({use: [nib()], compress: true}))
         .on('error', console.log)
-        .pipe(gulp.dest(dest));
+        .pipe(gulp.dest(dest))
+        .pipe(connect.reload());
 });
 
 gulp.task("jade", function () {
-    var src = 'examples/dev.jade';
+    var src = 'examples/**/*.jade';
     var dest = 'examples';
 
     return gulp.src([src])
         .pipe(changed(dest, {extension: '.html'}))
         .pipe(jade({pretty: true}))
         .on('error', console.log)
-        .pipe(gulp.dest(dest));
+        .pipe(gulp.dest(dest))
+        .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
@@ -40,7 +42,16 @@ gulp.task("build", function () {
     gulp.start("stylus");
 });
 
+gulp.task('webserver', function () {
+    connect.server({
+        root: './',
+        port: 8000,
+        livereload: true
+    });
+});
+
 gulp.task("default", function () {
     gulp.start("build");
+    gulp.start("webserver");
     gulp.start("watch");
 });
